f0ed7fd3738f1412a79690f1bcb0dd64fd4b14da
Anameskhi <ameskhi@unisens.ge>
2022-09-28 21:23:12 +0400

    quotes is security for another company employee, and public for common company employee

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,6 +6,7 @@ class Quote < ApplicationRecord
   after_create_commit -> { broadcast_prepend_to "quotes" }
   after_update_commit -> { broadcast_replace_to "quotes" }
   after_destroy_commit -> { broadcast_remove_to "quotes" }
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  belongs_to :company
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
 end
