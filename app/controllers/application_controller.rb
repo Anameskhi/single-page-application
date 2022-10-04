0aad7dd608fe04a787d973085bc3bfe1de2f2d33
Anameskhi <ameskhi@unisens.ge>
2022-10-04 17:06:12 +0400

    change after log out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
 
   private
-
+ 
   def current_company
     @current_company ||= current_user.company if user_signed_in?
   end
   helper_method :current_company
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
